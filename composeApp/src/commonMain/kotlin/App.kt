5d487cf0af7ff2d74d5a935b7a12490a1b97ad3d
Sergei Khorosh <khoroshsergei@gmail.com>
2024-06-17 23:40:04 +0300

    Build gradle update

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,20 +1,8 @@
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-import kotlinproject.composeapp.generated.resources.Res
-import kotlinproject.composeapp.generated.resources.compose_multiplatform
-import scenes.splash.SplashScreenViewModel
-import scenes.splashscreen.SplashScreen
+import Scenes.splash.SplashScreen
 
 @Composable
 @Preview
--- composeApp/src/commonMain/kotlin/Scenes/splash/SplashScreen.kt
+++ composeApp/src/commonMain/kotlin/Scenes/splash/SplashScreen.kt
@@ -1,17 +1,19 @@
-package scenes.splashscreen
+package scenes.splash
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
+import kotlinproject.composeapp.generated.resources.Res
+import kotlinproject.composeapp.generated.resources.compose_multiplatform
 import kotlinx.coroutines.delay
-import scenes.splash.SplashScreenViewModel
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun SplashScreen() {
@@ -36,8 +38,6 @@ fun SplashScreen() {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Blue),
-        contentAlignment = Alignment.Center
     ) {
         Text(
             text = "MetaSecret",
@@ -45,5 +45,13 @@ fun SplashScreen() {
             fontSize = 24.sp,
             textAlign = TextAlign.Center
         )
+
+        // Background image
+        Image(
+            painterResource(Res.drawable.compose_multiplatform),
+            contentDescription = "Background Image",
+        )
+
     }
 }
+
