25634bb06f124c0d73a2dd569cb14a78afc230e9
Sergei Khorosh <khoroshsergei@gmail.com>
2024-07-15 18:41:32 +0300

    Build gradle update

--- composeApp/src/commonMain/kotlin/splash/splash/SplashScreen.kt
+++ composeApp/src/commonMain/kotlin/splash/splash/SplashScreen.kt
@@ -1,5 +1,6 @@
-package scenes.splash
+package Scenes.splash
 
+import Onboarding.OnboardingScreen
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -13,6 +14,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
 import kotlinproject.composeapp.generated.resources.Res
 import kotlinproject.composeapp.generated.resources.background_logo
 import kotlinproject.composeapp.generated.resources.background_main
@@ -20,63 +23,69 @@ import kotlinproject.composeapp.generated.resources.logo
 import kotlinproject.composeapp.generated.resources.text
 import kotlinx.coroutines.delay
 import org.jetbrains.compose.resources.painterResource
+import scenes.splash.SplashScreenViewModel
 
+class SplashScreen : Screen {
+    @Composable
+    override fun Content() {
+        val viewModel = SplashScreenViewModel()
+        val navigator = LocalNavigator.current
+        LaunchedEffect(Unit) {
+            delay(3000)
+            navigator?.push(OnboardingScreen())
+            when {
+                viewModel.readOnboardingKey() -> {
+                    if (viewModel.checkAuth()) {
+                        // Route to main screen
+                    } else {
+                        // Route to Sign up
+                    }
+                }
 
-@Composable
-fun SplashScreen() {
-    val viewModel = SplashScreenViewModel()
+                else -> {
 
-    LaunchedEffect(Unit) {
-        delay(2000)
-        when {
-            viewModel.readOnboardingKey() -> {
-                if (viewModel.checkAuth() == true) {
-                    // Route to main screen
-                } else {
-                    // Route to Sign up
                 }
             }
-            else -> {
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center,
+        ) {
+            Image(
+                painter = painterResource(Res.drawable.background_main),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxSize(),
+                    contentScale = ContentScale.FillBounds
 
-            }
+            )
+            Image(
+                painter = painterResource(Res.drawable.background_logo),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(380.dp)
+                    .offset(y = (-25).dp)
+            )
+        }
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(Res.drawable.logo),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(115.dp)
+            )
+            Image(
+                painter = painterResource(Res.drawable.text),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(height = 23.dp, width = 184.dp)
+                    .offset(y = 25.dp)
+            )
         }
     }
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
-    ) {
-           Image(
-               painter = painterResource(Res.drawable.background_main),
-               contentDescription = null,
-               modifier = Modifier
-                   .fillMaxSize(),
-               contentScale = ContentScale.FillBounds
-           )
-           Image(
-               painter = painterResource(Res.drawable.background_logo),
-               contentDescription = null,
-               modifier = Modifier
-                   .size(213.dp)
-           )
-       }
-    Column (
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-           Image(
-               painter = painterResource(Res.drawable.logo),
-               contentDescription = null,
-               modifier = Modifier
-                   .size(115.dp)
-           )
-           Image(
-               painter = painterResource(Res.drawable.text),
-               contentDescription = null,
-               modifier = Modifier
-                   .size(height = 23.dp, width = 184.dp)
-                   .offset(y = 25.dp)
-           )
-       }
 }
-
