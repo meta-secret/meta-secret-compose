c02a65cb99a5e8d64fbfa23ab2bd87e6a7737fba
Sergei Khorosh <khoroshsergei@gmail.com>
2025-01-29 22:27:01 +0200

    Build gradle update

--- composeApp/src/iosMain/kotlin/sharedData/AppData.ios.kt
+++ composeApp/src/iosMain/kotlin/sharedData/AppData.ios.kt
@@ -4,3 +4,11 @@ package sharedData
 actual fun getAppVersion(): String {
     TODO("Not yet implemented")
 }
+
+actual fun getDeviceMake(): String {
+    TODO("Not yet implemented")
+}
+
+actual fun getDeviceId(): String {
+    TODO("Not yet implemented")
+}
