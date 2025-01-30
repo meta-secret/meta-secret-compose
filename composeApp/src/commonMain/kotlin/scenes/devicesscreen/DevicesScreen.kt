613e434e99da07f120dd1f10733839f1519b680a
Sergei Khorosh <khoroshsergei@gmail.com>
2025-01-31 00:20:10 +0200

    Build gradle update

--- composeApp/src/commonMain/kotlin/scenes/devicesscreen/DevicesScreen.kt
+++ composeApp/src/commonMain/kotlin/scenes/devicesscreen/DevicesScreen.kt
@@ -1,44 +1,18 @@
 package scenes.devicesscreen
 
-import AppColors
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.lifecycle.ViewModel
 import cafe.adriel.voyager.core.screen.Screen
 import kotlinproject.composeapp.generated.resources.Res
-import kotlinproject.composeapp.generated.resources.arrow
-import kotlinproject.composeapp.generated.resources.devices
 import kotlinproject.composeapp.generated.resources.devicesList
-import kotlinproject.composeapp.generated.resources.manrope_bold
-import kotlinproject.composeapp.generated.resources.manrope_regular
-import kotlinproject.composeapp.generated.resources.secret
-import org.jetbrains.compose.resources.Font
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
-import sharedData.getDeviceId
+import sharedData.bubbleContent
 import sharedData.warningContent
 import ui.CommonBackground
 
@@ -52,7 +26,7 @@ class DevicesScreen : Screen {
         CommonBackground(Res.string.devicesList) {
             warningContent(
                 viewModel = viewModel,
-                getDevicesCount = viewModel.data,
+                getDevicesCount = viewModel.dataDevices,
                 addDevice = { viewModel.addDevice() },
                 isVisible = isVisible,
             )
@@ -61,77 +35,13 @@ class DevicesScreen : Screen {
                     .fillMaxSize()
                     .padding(bottom = 80.dp)
             ) {
-                items(viewModel.data) { index ->
-                    deviceContent(viewModel, index)
+                items(viewModel.dataDevices) { index ->
+                    bubbleContent(
+                        viewModel = viewModel,
+                        getSecretsCount = viewModel.dataSecrets,
+                        getDevice = {viewModel.getDevice(index)}
+                    )
                 }
-            }
-        }
-    }
-}
-
-@Composable
-internal inline fun <reified T : ViewModel> deviceContent(viewModel: T, index: Int) {
-    if (viewModel !is DevicesScreenViewModel) {
-        throw IllegalArgumentException("Unsupported ViewModel type")
-    }
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = 16.dp, end = 16.dp, top = 16.dp)
-            .background(AppColors.White5, RoundedCornerShape(10.dp))
-            .height(92.dp)
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxSize()
-                .height(60.dp)
-                .padding(16.dp),
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
-        ) {
-            Image(
-                painter = painterResource(Res.drawable.devices),
-                contentDescription = null,
-                contentScale = ContentScale.FillBounds
-            )
-            Column {
-                Text(
-                    text = viewModel.getDevice(index).deviceMake + " (" + viewModel.getDevice(index).name + ")",
-                    style = TextStyle(
-                        fontSize = 16.sp,
-                        fontFamily = FontFamily(Font(Res.font.manrope_bold)),
-                        color = AppColors.White
-                    )
-                )
-                Text(
-                    text = getDeviceId(),
-                    style = TextStyle(
-                        fontSize = 11.sp,
-                        fontFamily = FontFamily(Font(Res.font.manrope_regular)),
-                        color = AppColors.White30
-                    )
-                )
-                Text(
-                    text = viewModel.getSecretsCount()
-                        .toString() + " " + stringResource(Res.string.secret),
-                    style = TextStyle(
-                        fontSize = 14.sp,
-                        fontFamily = FontFamily(Font(Res.font.manrope_regular)),
-                        color = AppColors.White75
-                    )
-                )
-            }
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .align(Alignment.CenterVertically)
-            ) {
-                Image(
-                    painter = painterResource(Res.drawable.arrow),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .align(Alignment.CenterEnd)
-                        .clickable { println("Pressed") }
-                )
             }
         }
     }
