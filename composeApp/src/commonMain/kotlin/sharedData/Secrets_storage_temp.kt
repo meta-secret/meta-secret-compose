109148eef9dec31c34e5619a7b9d3189a07022a6
Sergei Khorosh <khoroshsergei@gmail.com>
2025-01-31 17:46:53 +0200

    Build gradle update

--- composeApp/src/commonMain/kotlin/sharedData/Secrets_storage_temp.kt
+++ composeApp/src/commonMain/kotlin/sharedData/Secrets_storage_temp.kt
@@ -12,18 +12,10 @@ class SecretRepository(private val keyValueStorage: KeyValueStorage) {
 
     private val mutableSecretsList: MutableList<Secret> by lazy {
         mutableListOf(
-            Secret("Random Secret Name", "getPassword"),
-            Secret("Random Secret Name", "getPassword"),
-            Secret("Random Secret Name", "getPassword"),
-            Secret("Random Secret Name", "getPassword"),
-            Secret("Random Secret Name", "getPassword"),
-            Secret("Random Secret Name", "getPassword"),
-            Secret("Random Secret Name", "getPassword"),
-            Secret("Random Secret Name", "getPassword"),
-            Secret("Random Secret Name", "getPassword"),
-            Secret("Random Secret Name", "getPassword"),
-            Secret("Random Secret Name", "getPassword"),
-            Secret("Random Secret Name", "getPassword"),
+//            Secret("Random Secret Name", "getPassword"),
+//            Secret("Random Secret Name", "getPassword"),
+//            Secret("Random Secret Name", "getPassword"),
+//            Secret("Random Secret Name", "getPassword"),
         )
     }
     val secrets: List<Secret> get() = mutableSecretsList
