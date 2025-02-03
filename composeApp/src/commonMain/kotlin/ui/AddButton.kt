63c936688a23469f0f8c0e776b08f8f162388448
Sergei Khorosh <khoroshsergei@gmail.com>
2025-02-03 18:04:27 +0200

    Build gradle update
    Signed-off-by: Sergei Khorosh <khoroshsergei@gmail.com>

--- composeApp/src/commonMain/kotlin/ui/AddButton.kt
+++ composeApp/src/commonMain/kotlin/ui/AddButton.kt
@@ -1,32 +1,21 @@
 package ui
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import kotlinproject.composeapp.generated.resources.Res
 import kotlinproject.composeapp.generated.resources.addbutton
 import org.jetbrains.compose.resources.painterResource
 
-
 @Composable
-fun Addbutton(popUpHeader: String, boxHeight: Int) {
-    var showPopup by remember { mutableStateOf(false) }
+fun AddButton(action: () -> Unit) {
     val imgSize = 80
     Box(
         modifier = Modifier
@@ -39,24 +28,10 @@ fun Addbutton(popUpHeader: String, boxHeight: Int) {
             contentDescription = null,
             modifier = Modifier
                 .size(imgSize.dp)
-                .clickable {
-                    showPopup = true
-                }
+                .clickable { action() }
         )
     }
-    if (showPopup) {
-        popup(onDismiss = { showPopup = false }, popUpHeader, boxHeight)
-
-        AnimatedVisibility(
-            visible = showPopup,
-            enter = fadeIn(),
-            exit = fadeOut()
-        ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.Black.copy(alpha = 0.5f))
-            )
-        }
-    }
 }
+
+
+
