89e155e88af457d50fc451bde8eb497dc01a3ea7
Sergei Khorosh <khoroshsergei@gmail.com>
2025-02-03 18:04:27 +0200

    Build gradle update
    Signed-off-by: Sergei Khorosh <khoroshsergei@gmail.com>

--- composeApp/src/commonMain/kotlin/scenes/devicesscreen/DevicesScreen.kt
+++ composeApp/src/commonMain/kotlin/scenes/devicesscreen/DevicesScreen.kt
@@ -1,21 +1,27 @@
 package scenes.devicesscreen
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import kotlinproject.composeapp.generated.resources.Res
-import kotlinproject.composeapp.generated.resources.addDevice
 import kotlinproject.composeapp.generated.resources.devicesList
-import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
 import sharedData.enums.ScreenId
-import ui.Addbutton
+import ui.AddButton
 import ui.CommonBackground
 import ui.ContentCell
+import ui.DevicesStateHolder
+import ui.popUpDevice
 import ui.warningContent
 
 
@@ -23,7 +29,8 @@ class DevicesScreen : Screen {
     @Composable
     override fun Content() {
         val viewModel: DevicesScreenViewModel = koinViewModel()
-        val popUpHeader = stringResource(Res.string.addDevice)
+        val visibility by DevicesStateHolder.isDialogVisible.collectAsState()
+
 
         CommonBackground(Res.string.devicesList) {
             warningContent(
@@ -48,6 +55,22 @@ class DevicesScreen : Screen {
                 }
             }
         }
-        Addbutton(popUpHeader, 510)
+        AddButton {
+            DevicesStateHolder.setVisibility(true)
+        }
+        AnimatedVisibility(
+            visible = visibility,
+            enter = slideInVertically(
+                initialOffsetY = { it },
+                animationSpec = tween(durationMillis = 1500)
+            ),
+            exit = slideOutVertically(
+                targetOffsetY = { it },
+                animationSpec = tween(durationMillis = 1000)
+            )
+        ) {
+            popUpDevice()
+        }
     }
 }
+
